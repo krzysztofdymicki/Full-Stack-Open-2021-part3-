4f212ed4b134c5d72211981108d36ad6c4bd7e2c
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-08 16:52:07 +0200

    part3a finished

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-app.use(morgan('tiny'))
 app.use(express.json())
+morgan.token('content', (request,response) => JSON.stringify(request.body))
+app.use(morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.content(req,res)
+    ].join(' ')
+  })
+  )
 
 let persons = [
     {
