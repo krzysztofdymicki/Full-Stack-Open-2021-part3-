14362469de8c5f2f0768cb741d0ed7875eaf8096
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-08 16:01:42 +0200

    part3.3

--- index.js
+++ index.js
@@ -14,8 +14,16 @@ let persons = [
     }
 ]
 
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    person ? response.json(person)
+           : response.status(404).end()
 })
 
 app.get('/info', (request, response) => {
