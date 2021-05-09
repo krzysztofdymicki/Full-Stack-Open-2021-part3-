37698b5f26ed0002c645c1a585e2032de47ea310
unknown <krzysztof.dymicki.bp@gmail.com>
2021-05-09 11:46:38 +0200

    part3b preparing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 morgan.token('content', (request,response) => JSON.stringify(request.body))
 app.use(morgan((tokens, req, res) => {
